4b30f28cda67a606645556ef7f47e7efe6cdfd2d
krsconnie <coneh1702@gmail.com>
2025-11-21 19:34:51 -0300

    areglos menores

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h> // Necesario para usar true/false
+#include <stdbool.h> 
 #include "simulador.h"
 
 int main(int argc, char *argv[]) {
